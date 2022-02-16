75a9a041cb02055b97c811b156705e01d990551a
lowri h <479eleri@gmail.com>
2022-02-16 22:09:49 +0000

    slightly better form name

--- dnd distance calculator/Program.cs
+++ dnd distance calculator/Program.cs
@@ -17,7 +17,7 @@ namespace dnd_distance_calculator
             DataImport.ExcelImport();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
